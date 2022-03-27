0916f579ea4ecdb16864378976a8932ae841a3c5
YarGromov <1l1l1arr1l1l1@gmail.com>
2022-03-27 18:47:56 +0200

    symbol

--- js/script.js
+++ js/script.js
@@ -4,61 +4,84 @@ const screen = document.querySelector(".screen");
 
 btns.forEach((btn) => {
   btn.addEventListener("click", function (event) {
-   
     let target = event.target;
 
     let num;
-    let num2;
+    let symbol;
 
-    let div_screen = document.createElement('div');
+    let div_screen = document.createElement("div");
     screen.append(div_screen);
 
-    
-    if(target.innerHTML == '1') {
+    if (target.innerHTML == "1") {
       num = 1;
-      div_screen.append(num)
-      console.log(num)
-    } else if(target.innerHTML == '2') {
+      div_screen.append(num);
+      console.log(num);
+    } else if (target.innerHTML == "2") {
       num = 2;
-      div_screen.append(num)
-      console.log(num)
-    } else if(target.innerHTML == '3') {
+      div_screen.append(num);
+      console.log(num);
+    } else if (target.innerHTML == "3") {
       num = 3;
-      div_screen.append(num)
-      console.log(num)
-    } else if(target.innerHTML == '4') {
+      div_screen.append(num);
+      console.log(num);
+    } else if (target.innerHTML == "4") {
       num = 4;
-      div_screen.append(num)
-      console.log(num)
-    } else if(target.innerHTML == '5') {
+      div_screen.append(num);
+      console.log(num);
+    } else if (target.innerHTML == "5") {
       num = 5;
-      div_screen.append(num)
-      console.log(num)
-    } else if(target.innerHTML == '6') {
+      div_screen.append(num);
+      console.log(num);
+    } else if (target.innerHTML == "6") {
       num = 6;
-      div_screen.append(num)
-      console.log(num)
-    } else if(target.innerHTML == '7') {
+      div_screen.append(num);
+      console.log(num);
+    } else if (target.innerHTML == "7") {
       num = 7;
-      div_screen.append(num)
-      console.log(num)
-    } else if(target.innerHTML == '8') {
+      div_screen.append(num);
+      console.log(num);
+    } else if (target.innerHTML == "8") {
       num = 8;
-      div_screen.append(num)
-      console.log(num)
-    } else if(target.innerHTML == '9') {
+      div_screen.append(num);
+      console.log(num);
+    } else if (target.innerHTML == "9") {
       num = 9;
-      div_screen.append(num)
-      console.log(num)
-    } else if(target.innerHTML == '0') {
+      div_screen.append(num);
+      console.log(num);
+    } else if (target.innerHTML == "0") {
       num = 0;
-      div_screen.append(num)
-      console.log(num)
+      div_screen.append(num);
+      console.log(num);
+    } else if (target.innerHTML == ".") {
+      num = ".";
+      div_screen.append(num);
+      console.log(num);
     }
- 
-    
-    
 
-
+    if (target.matches(".plus-minus")) {
+      symbol = "+/-";
+      console.log(symbol);
+    } else if (target.matches(".arrow")) {
+      symbol = "<=";
+      console.log(symbol);
+    } else if (target.matches(".mult")) {
+      symbol = "*";
+      console.log(symbol);
+    } else if (target.matches(".divide")) {
+      symbol = "/";
+      console.log(symbol);
+    } else if (target.matches(".minus")) {
+      symbol = "-";
+      console.log(symbol);
+    } else if (target.matches(".plus")) {
+      symbol = "+";
+      console.log(symbol);
+    } else if (target.matches(".equality")) {
+      symbol = "=";
+      console.log(symbol);
+    } else if (target.matches(".C_C")) {
+      symbol = "C";
+      console.log(symbol);
+    }
   });
 });
