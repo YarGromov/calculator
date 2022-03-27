e98a639da6288aefe1e4a2c725214d148da71e3e
YarGromov <1l1l1arr1l1l1@gmail.com>
2022-03-27 15:16:51 +0200

    addEventListener

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
-const btns = document.querySelectorAll('button');
+const btns = document.querySelectorAll("button");
+const btn = document.querySelector("btn");
 const screen = document.getElementsByClassName(".screen");
 
-btns.addEventListener('click', (event) => {
-
-console.log('lol')
+btns.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    
+  });
 });
-
